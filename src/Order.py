e2efb4986b909c48414d9d7108d9e5483b7d9188
JudeBake <julien.bacon.1@gmail.com>
2014-01-12 20:31:42 -0500

    finishing the loading of the data from xls files

--- src/Order.py
+++ src/Order.py
@@ -4,9 +4,12 @@ Created on 2014-01-09
 @author: julienbacon
 '''
 
+from collections import deque
+from datetime import date
 from ProductOrder import ProductOrder
 import os
-from xlrd import open_workbook
+from xlrd import open_workbook, XL_CELL_EMPTY, XL_CELL_TEXT, XL_CELL_NUMBER
+import xlutils
 from xlwt import Workbook
 
 class OrderError(Exception):
@@ -16,7 +19,7 @@ class OrderError(Exception):
 
 class Order:
     '''
-    Order is a wrapper of a list of ProductOrders.
+    Order is a list of ProductOrders.
     It creates its list from an excel file. If the file was not modify earlier
     than yesterday, Order assumes that there's nothing new to order.
     
@@ -30,33 +33,88 @@ class Order:
     __ORDERED_SH_NAME = 'ordere'
     __TITLE_ROW = 0
     __1ST_DATA_ROW = 1
-    __PROD_NB_TITLE = 'Produt Number'
-    __DESC_TITLE = 'Description'
-    __QTY_TITLE = 'Quantity'
-    __DATE_TILE = 'Date'
+    __LAST_DATA_ROW = 13
+    __PROD_NB_TITLE = 'no. produit'
+    __DESC_TITLE = 'description'
+    __QTY_TITLE = 'quantite'
+    __DATE_TILE = 'date'
+    __EMPLOYEE_TITLE = 'commis'
+    __TITLE_MAP = (__PROD_NB_TITLE, __DESC_TITLE, __QTY_TITLE, __DATE_TILE,
+                   __EMPLOYEE_TITLE)
+    __EOD_ROW_MARKER = 'EOD'
     
-    def __colMappingBuilder(self, sheet):
-        colMapping = {self.__PROD_NB_TITLE:list(), self.__DESC_TITLE:list(),
-                      self.__QTY_TITLE:list(), self.__DATE_TILE:list()}
+    def __colMapping(self, sheet):
+        #parse the col. to figure which one has what data
+        tempList = list()
         for col in range(sheet.ncols):
-            for title in colMapping.keys():
+            for title in self.__TITLE_MAP:
                 if sheet.cell(self.__TITLE_ROW, col).value == title:
-                    colMapping[title].append(col)
-        return colMapping
+                    tempList.append(col)
+                    if title == self.__EMPLOYEE_TITLE:
+                        self.__colMap.append(tempList)
+                        tempList = list()
     
-    def __processOrder(self, sheet):
-        for row in range(self.__1ST_DATA_ROW, sheet.nrows):
-            for col in (self.__PROD_NB_COL, self.__DESC_COL, self.__QTY_COL,
-                        self.__DATE_COL):
-                value = sheet.cell(row, col).value
+    def __rowMapping(self, sheet):
+        flagedAsData = False
+        #for each order bloc
+        for orderBloc in self.__colMap:
+            #parse the row to figure which one containt data (partial or complete)
+            tempList = list()
+            for row in range(self.__1ST_DATA_ROW, self.__LAST_DATA_ROW + 1):
+                for col in orderBloc:
+                    if sheet.cell(row, col).value:
+                        flagedAsData = True
+                if flagedAsData:
+                    tempList.append(row)
+                    flagedAsData = False
+            self.__rowMap.append(tempList)
+            tempList = list()
+    
+    def __processLoading(self, sheet, withDateFlag):
+        #for each order bloc, load the data
+        for orderBloc in self.__colMap:
+            #if there's a date in the file, construct the order list with the
+            #date from the file
+            currentDate = date.today()
+            for row in self.__rowMap.popleft():
+                prodOrder = ProductOrder()
+                if withDateFlag:
+                    (prodNbCol, qtyCol, descCol, dateCol, employeeCol) = orderBloc
+                    if sheet.cell(row, qtyCol).ctype == XL_CELL_TEXT:
+                        prodOrder[ProductOrder.DATE_KEY] = \
+                            str(sheet.cell(row, dateCol).value)
+                else:
+                    (prodNbCol, qtyCol, descCol, employeeCol) = orderBloc
+                if sheet.cell(row, prodNbCol).ctype == XL_CELL_NUMBER:
+                    prodOrder[ProductOrder.PROD_NB_KEY] = \
+                        int(sheet.cell(row, prodNbCol).value)
+                if sheet.cell(row, qtyCol).ctype == XL_CELL_NUMBER:
+                    prodOrder[ProductOrder.QTY_TO_ORDER_KEY] = \
+                        int(sheet.cell(row, qtyCol).value)
+                if sheet.cell(row, qtyCol).ctype == XL_CELL_TEXT:
+                    prodOrder[ProductOrder.DESC_KEY] = \
+                        str(sheet.cell(row, descCol).value)
+                    prodOrder[ProductOrder.DATE_KEY] = currentDate
+                if sheet.cell(row, qtyCol).ctype == XL_CELL_TEXT:
+                    prodOrder[ProductOrder.EMPLOYEE_KEY] = \
+                        str(sheet.cell(row, employeeCol).value)
+                self.__oderList.append(prodOrder)
     
     def __init__(self):
         '''
         Constructor
         '''
-        self.__oderList = []
+        self.__oderList = deque()
+        self.__colMap = []
+        self.__rowMap = deque()
         
-    def loadOrder(self, sourceFile):
+    def __len__(self):
+        '''
+        Get the length of the actual ProductOrder list.
+        '''
+        return len(self.__oderList)
+        
+    def loadOrder(self, sourceFile, withDateFlag = False):
         '''
         Load an Order in read only mode.
         '''
@@ -64,16 +122,27 @@ class Order:
             book = open_workbook(sourceFile, on_demand = True)
         except:
             pass
-        sheet = book.sheet_by_name(self.__DAILY_ORDER_SH_NAME)
+        sheet = book.sheet_by_index(0)
         #figure if there's something new to order
         if sheet.nrows == 1:
             pass #raise exception
-        self.__processOrder(sheet)
-  
+        #build the map of the data
+        self.__colMapping(sheet)
+        self.__rowMapping(sheet)
+        #process the data
+        self.__processLoading(sheet, withDateFlag)
+        book.release_resources()
         
-    def getOrderList(self):
+    def popLeft(self):
         '''
-        Get the ProductOrderList.
+        Pop left the ProductOrder list.
         '''
-        return self.__oderList
+        return self.__oderList.popleft()
+    
+    def popRight(self):
+        '''
+        Pop right th ProductOrder list.
+        '''
+        return self.__oderList.pop()
+        
         
--- src/OrderTest.py
+++ src/OrderTest.py
@@ -10,30 +10,59 @@ from Order import Order
 import os
 import unittest
 
-TEST_FILE_ROOT = '..\test_files'
+TEST_FILE_ROOT = r'..\test_files'
 
-def productOrderBuilder(prodNb, desc, qty, date):
+def productOrderBuilder(prodNb, desc, qty, date, employee):
     prodOrder = ProductOrder()
     prodOrder[ProductOrder.PROD_NB_KEY] = prodNb
     prodOrder[ProductOrder.DESC_KEY] = desc
     prodOrder[ProductOrder.QTY_TO_ORDER_KEY] = qty
     prodOrder[ProductOrder.DATE_KEY] = date
+    prodOrder[ProductOrder.EMPLOYEE_KEY] = employee
     return prodOrder
     
 class OrderLoadingTest(unittest.TestCase):
-    ATTENDED_RESULTS = [productOrderBuilder(133045, 'allo', 100, date.today().strftime("%d/%m/%y")),
-                        productOrderBuilder(20432, 'bonjour', 2000, date.today().strftime("%d/%m/%y"))]
-    
-    __order = Order()
+    ATTENDED_RESULTS1 = [productOrderBuilder(133045, 'allo', 100, date.today(), 'jb'),
+                         productOrderBuilder(203432, 'bonjour', 2000, date.today(), 'jf'),
+                         productOrderBuilder(130090, '', None, date.today(), ''),
+                         productOrderBuilder(133840, 'dodo', 50, date.today(), 'justin'),
+                         productOrderBuilder(130050, 'coucou', None, date.today(), 'Daniel'),
+                         productOrderBuilder(None, '', 200, date.today(), '')]
+    ATTENDED_RESULTS2 = [productOrderBuilder(None, '', 200, date(1232, 4, 13), ''),
+                         productOrderBuilder(130050, 'coucou', None, date(1231, 4, 13), 'Daniel'),
+                         productOrderBuilder(133840, 'dodo', 50, date(1201, 1, 1), 'justin'),
+                         productOrderBuilder(130090, '', None, date(1201, 1, 1), ''),
+                         productOrderBuilder(203432, 'bonjour', 2000, date(1201, 1, 1), 'jf'),
+                         productOrderBuilder(133045, 'allo', 100, date(1201, 1, 1), 'jb')]
+    __orderWODate = Order()
+    __orderWDate = Order()
 
-    def testOrderLoading(self):
+    def testOrderWODateLoading(self):
         '''
-        Order must be able to load its ProductOrders from an excel file in read
-        only mode and get its whole ProductOrder list.
+        Order must be able to load its ProductOrders from an excel file without
+        the date in it, get its length and pop left its whole ProductOrder list.
         '''
-        self.__order.loadOrder(os.path.join((TEST_FILE_ROOT, self.__name__)))
-        result = self.__order.getOrderList()
-        self.assertListEqual(result, self.ATTENDED_RESULTS, 'Order failed to its ProductOrder list from an excel file and get its entire ProductOrder list')
+        self.__orderWODate.loadOrder(os.path.join(TEST_FILE_ROOT, 'OrderTestFile1.xls'), False)
+        result = []
+        for i in range(len(self.__orderWODate)):
+            result.append(self.__orderWODate.popLeft())
+        i = i
+        self.assertListEqual(result, self.ATTENDED_RESULTS1, 'Order failed to its ProductOrder list from an excel file, get its length and popLeft its whole ProductOrder list')
+        
+    def testOrderWDateLoading(self):
+        '''
+        Order must be able to load its ProductOrders from an excel file wit
+        the date in it, get its length and pop right its whole ProductOrder list.
+        '''
+        self.__orderWDate.loadOrder(os.path.join(TEST_FILE_ROOT, 'OrderTestFile2.xls'), True)
+        result = []
+        for i in range(len(self.__orderWDate)):
+            result.append(self.__orderWDate.popRight())
+        i = i
+        self.assertListEqual(result, self.ATTENDED_RESULTS2, 'Order failed to its ProductOrder list from an excel file, get its length and popRight its whole ProductOrder list')
+        
+class filterOderTest(unittest.TestCase):
+    ATTENDED_RESULTS = []
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
