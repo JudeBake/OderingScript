a877aa6bbda2392475cb5ed30711bfc8c33e8d00
JudeBake <julien.bacon.1@gmail.com>
2014-01-15 12:45:15 -0500

    adding Log class

---
+++ src/Log.py
@@ -0,0 +1,50 @@
+'''
+Created on 2014-01-14
+
+@author: julienbacon
+'''
+
+from datetime import date
+import os
+
+class Log:
+    '''
+    Log is a list of messages about what happens during the OrderingScript
+    process.
+    
+    It provides the following interfaces:
+        - Log a message in the list.
+        - Save its message list in a text file.
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.__msgList = list()
+        
+    def logMsg(self, msg):
+        '''
+        Log new messages.
+        '''
+        self.__msgList.append(msg)
+        
+    def getMsgList(self):
+        '''
+        Get the whole message list.
+        '''
+        return self.__msgList
+        
+    def outputLog(self, destinationPath):
+        '''
+        Output the log messages at the desired location.
+        '''
+        currentDate = date.today()
+        logFile = open(os.path.join(destinationPath, 'Log_' + \
+                                    currentDate.strftime('%d-%m-%Y')) +'.txt', 'w+')
+        for msg in self.__msgList:
+            logFile.write(msg + '\n\r')
+        logFile.flush()
+        logFile.close()
+        
+        
---
+++ src/LogTest.py
@@ -0,0 +1,48 @@
+'''
+Created on 2014-01-14
+
+@author: julienbacon
+'''
+
+from datetime import date
+from Log import Log
+import os
+import unittest
+
+TEST_FILE_ROOT = r'..\test_files'
+
+class logMsgTest(unittest.TestCase):
+    TEST_VALUES = ['allo', 'bonjour', 'comment ca va']
+
+    def testLogMsg(self):
+        '''
+        Log must be able to log new messages.
+        '''
+        log = Log()
+        for msg in self.TEST_VALUES:
+            log.logMsg(msg)
+        self.assertListEqual(log.getMsgList(), self.TEST_VALUES, 'Log isn\'t able to log new messages')
+        
+class outputLogTest(unittest.TestCase):
+    TEST_VALUES = ['eugab', ';equbfa', 'uiqefajbaiu', 'abufuif;avn ']
+    
+    def tearDown(self):
+        currentDate = date.today()
+        os.remove(os.path.join(TEST_FILE_ROOT, 'Log_' + currentDate.strftime('%d-%m-%Y') + '.txt'))
+        
+    def testOutputLog(self):
+        '''
+        Log must be able to output its message list.
+        '''
+        currentDate = date.today()
+        log = Log()
+        for msg in self.TEST_VALUES:
+            log.logMsg(msg)
+        log.outputLog(TEST_FILE_ROOT)
+        f = open(os.path.join(TEST_FILE_ROOT, 'Log_' + currentDate.strftime('%d-%m-%Y') + '.txt'), 'r')
+        result = f.readlines()
+        self.assertListEqual(result, self.TEST_VALUES, 'log isn\'t able to output its message list.')
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
